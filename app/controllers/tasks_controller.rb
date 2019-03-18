a87caada726deee2c69aa0977805c30db81e8f7d
yuma.sakata <sakachan19801980@gmail.com>
2019-03-18 13:42:08 +0000

    ninsyo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
       @tasks = Task.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+ root to: 'toppages#index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
