fb4eb6cec7059d463cb5b1347023f1db1435e743
yuma.sakata <sakachan19801980@gmail.com>
2019-04-20 00:58:55 +0000

    for heroku2

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190414120946) do
-
-  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "status"
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20190420004828) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -29,5 +20,4 @@ ActiveRecord::Schema.define(version: 20190414120946) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "tasks", "users"
 end
